ba102fcf43097e0cedb60c402c91d1b52eca8ff2
NishantAhirao <ahiraonishant@gmail.com>
2026-06-21 14:57:36 +0530

    latest-updates-21-06-2026

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,61 +1,90 @@
-import React from 'react'
-import './intro.css'
+import React from "react";
+import "./intro.css";
 // import bg from '../../assets/image.png'
-import bg from '../../assets/Nishant_wobg01.png'
+import bg from "../../assets/Nishant_wobg01.png";
 // import btnImg from '../../assets/hireme.png'
 // import btnImg2 from '../../assets/file.png'
 
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 const Intro = () => {
-  
-const [typeEffect]= useTypewriter({
-words: ['CSE Undergrad ', 'DSA Enthusiast', 'Learner', 'Web Developer'] ,
-loop: {},
-typeSpeed: 100,
-deleteSpeed:40})
+  const [typeEffect] = useTypewriter({
+    words: [
+      "Backend Developer",
+      "Spring Boot Developer",
+      "Learner",
+      "DSA Enthusiast",
+    ],
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed: 40,
+  });
   const handleDownload = () => {
-    
-    const pdfUrl = 'https://drive.google.com/file/d/1yw4tyUr82x6znDh3iXmim8gTD8jCRHM4/view?usp=sharing';
+    const pdfUrl =
+      "https://drive.google.com/file/d/1aqh41g3d_8c7xaNzbPXl86c7YGX3p7h4/view?usp=sharing";
 
-    window.open(pdfUrl, '_blank');
+    window.open(pdfUrl, "_blank");
   };
 
   return (
-    <section id='intro'>
-        
-
-        
-        <div className='IntroContent'>
-          <div className='textCont'>
-
-          
-            <span className='hello'>Hello, This is me</span>
-            <span className='IntroText'>Nishant Ahirao</span>
-            <span className='hello2'>& I'm a <span className='typing'>{typeEffect}</span ><span style={{color:"#ffbf00"}}><Cursor className='cursors' ></Cursor></span></span>
-            <p className='IntroPara'>pursuing bachelor's in computer engineering. Working on full stack / MERN development.</p>
-            </div>
-            <div className='introBtns'>
-              <Link to=''><button className='btn' onClick={handleDownload}><i className="uil uil-file-download-alt unicons"></i>Download CV</button></Link>
-
-              <Link to='contact' spy={true} smooth={true}  offset={-50} duration={500}><button className='btn'><i className="uil uil-briefcase-alt unicons"></i>Hire Me</button></Link>
-            </div>
-            <div className='introBtns2'>
-              <a href='https://www.linkedin.com/in/nishant-ahirao-036a12220/' target='_blank' rel="noreferrer"><i className="uil uil-linkedin unicons2"></i></a>
-              <a href='https://github.com/NishantAhirao' target='_blank' rel="noreferrer"><i className="uil uil-github unicons2"></i></a>
-            
-            
-            </div>
-            
-            
+    <section id="intro">
+      <div className="IntroContent">
+        <div className="textCont">
+          <span className="hello">Hello, This is me</span>
+          <span className="IntroText">Nishant Ahirao</span>
+          <span className="hello2">
+            & I'm a <span className="typing">{typeEffect}</span>
+            <span style={{ color: "#ffbf00" }}>
+              <Cursor className="cursors"></Cursor>
+            </span>
+          </span>
+          <p className="IntroPara">
+            Experienced in building scalable Java & Spring Boot microservices
+            for financial trading systems.
+          </p>
         </div>
-        <div className='circle'>
-        <img src={bg} alt='' className='bg'></img>
+        <div className="introBtns">
+          <Link to="">
+            <button className="btn" onClick={handleDownload}>
+              <i className="uil uil-file-download-alt unicons"></i>Download CV
+            </button>
+          </Link>
+
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
+            <button className="btn">
+              <i className="uil uil-briefcase-alt unicons"></i>Hire Me
+            </button>
+          </Link>
         </div>
-        
+        <div className="introBtns2">
+          <a
+            href="https://www.linkedin.com/in/nishant-ahirao-036a12220/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-linkedin unicons2"></i>
+          </a>
+          <a
+            href="https://github.com/NishantAhirao"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-github unicons2"></i>
+          </a>
+        </div>
+      </div>
+      <div className="circle">
+        <img src={bg} alt="" className="bg"></img>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,53 +1,271 @@
-import React from 'react'
-import './skills.css'
+import React from "react";
+import "./skills.css";
 
 // import { SiPowerbi } from 'react-icons/si';
 
+import reacticon from "../../assets/react.png";
+import cppicon from "../../assets/c-.png";
+import powericon from "../../assets/Power-BI.png";
+import javaicon from "../../assets/java.png";
+import pythonicon from "../../assets/python.png";
+import htmlicon from "../../assets/html-5.png";
+import cssicon from "../../assets/css-3.png";
+import mysqlicon from "../../assets/mySQL.png";
+import jsicon from "../../assets/js.png";
+import sqlicon from "../../assets/sql-server.png";
 
-import reacticon from '../../assets/react.png'
-import cppicon from '../../assets/c-.png'
-import powericon from '../../assets/Power-BI.png'
-import javaicon from '../../assets/java.png'
-import pythonicon from '../../assets/python.png'
-import htmlicon from '../../assets/html-5.png'
-import cssicon from '../../assets/css-3.png'
-import mysqlicon from '../../assets/mySQL.png'
-import jsicon from '../../assets/js.png'
-import sqlicon from '../../assets/sql-server.png'
-import probsol from '../../assets/problem-solving-skills.png'
-import frontendicon from '../../assets/frontend.png'
+// Backend
+import springbooticon from "../../assets/spring-boot.png";
+import springsecurityicon from "../../assets/spring-security.svg";
+import restapiicon from "../../assets/rest-api.png";
+import microservicesicon from "../../assets/microservice.png";
+import jpaicon from "../../assets/jpa.svg";
+import jdbcicon from "../../assets/database.png";
+
+// Frontend
+import bootstrapicon from "../../assets/bootstrap.png";
+
+// Database & Cloud
+import oracleicon from "../../assets/oracle.png";
+import redisicon from "../../assets/redis.png";
+import awsicon from "../../assets/aws.png";
+import plsqlicon from "../../assets/plsql.png";
+
+// Tools
+import giticon from "../../assets/git.png";
+import mavenicon from "../../assets/maven.png";
+import postmanicon from "../../assets/postman.png";
+import swaggericon from "../../assets/swagger.png";
+import linuxicon from "../../assets/linux.png";
+
+// CS Fundamentals
+import multithreadingicon from "../../assets/multithreading.png";
+import dsaicon from "../../assets/dsa.png";
+import oopicon from "../../assets/oops.png";
+import dbmsicon from "../../assets/database.png";
+import osicon from "../../assets/operation-systems.png";
+import networkicon from "../../assets/computer-networks.png";
+
+// Domain
+import tradingicon from "../../assets/trading.png";
+
+import paymenticon from "../../assets/payment.png";
 
 const Skills = () => {
+  // const skillsData = [
+  //   { skill: "C++", icon: <img className='skillsIcons' src={cppicon} alt="cpp"/> },
+  //   { skill: "Python", icon: <img className='skillsIcons' src={pythonicon} alt="python"/> },
+  //   { skill: "Java", icon: <img className='skillsIcons' src={javaicon} alt="js"/> },
+  //   { skill: "Power BI", icon: <img className='skillsIcons' src={powericon} alt="powerBI"/> },
+  //   { skill: "SQL", icon: <img className='skillsIcons' src={sqlicon} alt="sql"/> },
+  //   { skill: "mySQL", icon: <img className='skillsIcons' src={mysqlicon} alt="mySQL"/> },
+  //   { skill: "Front-End Web Development", icon: <img className='skillsIcons' src={frontendicon} alt="frontend"/>},
+  //   { skill: "Problem Solving", icon: <img className='skillsIcons' src={probsol} alt="problem-solving"/> },
+  //   { skill: "HTML5", icon: <img className='skillsIcons' src={htmlicon} alt="html"/>},
+  //   { skill: "CSS", icon: <img className='skillsIcons' src={cssicon} alt="css"/>},
+  //   { skill: "Javascript", icon: <img className='skillsIcons' src={jsicon} alt="js"/>  },
+  //   { skill: "ReactJS", icon: <img className='skillsIcons' src={reacticon} alt="reacticon"/> },
+  // ];
 
   const skillsData = [
-    { skill: "C++", icon: <img className='skillsIcons' src={cppicon} alt="cpp"/> },
-    { skill: "Python", icon: <img className='skillsIcons' src={pythonicon} alt="python"/> },
-    { skill: "Java", icon: <img className='skillsIcons' src={javaicon} alt="js"/> },
-    { skill: "Power BI", icon: <img className='skillsIcons' src={powericon} alt="powerBI"/> },
-    { skill: "SQL", icon: <img className='skillsIcons' src={sqlicon} alt="sql"/> },
-    { skill: "mySQL", icon: <img className='skillsIcons' src={mysqlicon} alt="mySQL"/> },
-    { skill: "Front-End Web Development", icon: <img className='skillsIcons' src={frontendicon} alt="frontend"/>},
-    { skill: "Problem Solving", icon: <img className='skillsIcons' src={probsol} alt="problem-solving"/> },
-    { skill: "HTML5", icon: <img className='skillsIcons' src={htmlicon} alt="html"/>},
-    { skill: "CSS", icon: <img className='skillsIcons' src={cssicon} alt="css"/>},
-    { skill: "Javascript", icon: <img className='skillsIcons' src={jsicon} alt="js"/>  },
-    { skill: "ReactJS", icon: <img className='skillsIcons' src={reacticon} alt="reacticon"/> },
+    // Languages
+    {
+      skill: "Java",
+      icon: <img className="skillsIcons" src={javaicon} alt="java" />,
+    },
+
+    // Backend
+    {
+      skill: "Spring Boot",
+      icon: (
+        <img className="skillsIcons" src={springbooticon} alt="spring-boot" />
+      ),
+    },
+    {
+      skill: "Microservices",
+      icon: (
+        <img
+          className="skillsIcons"
+          src={microservicesicon}
+          alt="microservices"
+        />
+      ),
+    },
+    {
+      skill: "REST APIs",
+      icon: <img className="skillsIcons" src={restapiicon} alt="rest-apis" />,
+    },
+    {
+      skill: "Spring Security",
+      icon: (
+        <img
+          className="skillsIcons"
+          src={springsecurityicon}
+          alt="spring-security"
+        />
+      ),
+    },
+    {
+      skill: "Spring Data JPA",
+      icon: <img className="skillsIcons" src={jpaicon} alt="spring-data-jpa" />,
+    },
+    {
+      skill: "JDBC",
+      icon: <img className="skillsIcons" src={jdbcicon} alt="jdbc" />,
+    },
+
+    // Frontend
+    {
+      skill: "React",
+      icon: <img className="skillsIcons" src={reacticon} alt="react" />,
+    },
+    {
+      skill: "Bootstrap",
+      icon: <img className="skillsIcons" src={bootstrapicon} alt="bootstrap" />,
+    },
+    {
+      skill: "HTML5",
+      icon: <img className="skillsIcons" src={htmlicon} alt="html5" />,
+    },
+    {
+      skill: "CSS3",
+      icon: <img className="skillsIcons" src={cssicon} alt="css3" />,
+    },
+
+    // Database & Cloud
+    {
+      skill: "Oracle",
+      icon: <img className="skillsIcons" src={oracleicon} alt="oracle" />,
+    },
+    {
+      skill: "PL/SQL",
+      icon: <img className="skillsIcons" src={plsqlicon} alt="pl-sql" />,
+    },
+    {
+      skill: "MySQL",
+      icon: <img className="skillsIcons" src={mysqlicon} alt="mysql" />,
+    },
+    {
+      skill: "Redis",
+      icon: <img className="skillsIcons" src={redisicon} alt="redis" />,
+    },
+    {
+      skill: "AWS",
+      icon: <img className="skillsIcons" src={awsicon} alt="aws" />,
+    },
+
+    // Tools
+    {
+      skill: "Git",
+      icon: <img className="skillsIcons" src={giticon} alt="git" />,
+    },
+    {
+      skill: "Maven",
+      icon: <img className="skillsIcons" src={mavenicon} alt="maven" />,
+    },
+    {
+      skill: "Postman",
+      icon: <img className="skillsIcons" src={postmanicon} alt="postman" />,
+    },
+    {
+      skill: "Swagger",
+      icon: <img className="skillsIcons" src={swaggericon} alt="swagger" />,
+    },
+    {
+      skill: "Linux",
+      icon: <img className="skillsIcons" src={linuxicon} alt="linux" />,
+    },
+
+    {
+      skill: "Python",
+      icon: <img className="skillsIcons" src={pythonicon} alt="python" />,
+    },
+    {
+      skill: "C/C++",
+      icon: <img className="skillsIcons" src={cppicon} alt="cpp" />,
+    },
+    {
+      skill: "JavaScript",
+      icon: <img className="skillsIcons" src={jsicon} alt="javascript" />,
+    },
+    {
+      skill: "SQL",
+      icon: <img className="skillsIcons" src={sqlicon} alt="sql" />,
+    },
+
+    // Concepts
+    {
+      skill: "Multithreading",
+      icon: (
+        <img
+          className="skillsIcons"
+          src={multithreadingicon}
+          alt="multithreading"
+        />
+      ),
+    },
+    {
+      skill: "DSA",
+      icon: <img className="skillsIcons" src={dsaicon} alt="dsa" />,
+    },
+    {
+      skill: "OOP",
+      icon: <img className="skillsIcons" src={oopicon} alt="oop" />,
+    },
+    {
+      skill: "DBMS",
+      icon: <img className="skillsIcons" src={dbmsicon} alt="dbms" />,
+    },
+    {
+      skill: "Operating Systems",
+      icon: (
+        <img className="skillsIcons" src={osicon} alt="operating-systems" />
+      ),
+    },
+    {
+      skill: "Computer Networks",
+      icon: (
+        <img
+          className="skillsIcons"
+          src={networkicon}
+          alt="computer-networks"
+        />
+      ),
+    },
+
+    // Domain
+    {
+      skill: "Securities Trading",
+      icon: (
+        <img
+          className="skillsIcons"
+          src={tradingicon}
+          alt="securities-trading"
+        />
+      ),
+    },
+    {
+      skill: "Payment Gateways",
+      icon: (
+        <img className="skillsIcons" src={paymenticon} alt="payment-gateways" />
+      ),
+    },
   ];
 
-
   return (
-    <section id='skills'>
-      <p className='skillsIntro'>Here are some of</p>
-      <h2 className='skillsTitle '>My Skills</h2>
-      <ul className='mySkillsList '>
-        {
-          skillsData.map((item, index)=>(
-            <li key={index} className='singleSkill'>{item.icon}{item.skill}</li>
-          ))
-        }
+    <section id="skills">
+      <p className="skillsIntro">Here are some of</p>
+      <h2 className="skillsTitle ">My Skills</h2>
+      <ul className="mySkillsList ">
+        {skillsData.map((item, index) => (
+          <li key={index} className="singleSkill">
+            {item.icon}
+            {item.skill}
+          </li>
+        ))}
       </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
